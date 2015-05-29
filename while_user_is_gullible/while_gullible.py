858cee0041818b1d36a64ef14d40496b5768062b
Josien Braas <j.a.braas@hotmail.com>
2015-05-29 21:19:34 +0200

    Added first modification to gullible user exercise.

--- while_user_is_gullible/while_gullible.py
+++ while_user_is_gullible/while_gullible.py
@@ -4,14 +4,20 @@
 
 def ask_number():
     """Ask the user for a number."""
-    while True:
+    counter = 1
+    while counter <= 10:
         number = input("\nHello! Please provide any number but 5: ")
         try:
             if int(number) == 5:
                 print("Hey! You weren't supposed to enter 5!")
                 break
         except ValueError:
-            print("Hey! That's not a number!")
+            # print("Hey! That's not a number...")
+            pass
+        if counter == 10:
+            print("Wow, you're more patient than I am, you win.")
+            break
+        counter += 1
 
 if __name__ == "__main__":
     ask_number()
